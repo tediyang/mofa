b2c5edfe96d821d0b3df6f5fd2c6bd420a2bed2e
Eyand Daniel Eyoh <daniel.eyang.ed@gmail.com>
2025-06-11 14:44:05 +0100

    UPDATES: removed dropdown function from dashboard

--- admin/js/dashboard-admin.js
+++ admin/js/dashboard-admin.js
@@ -1,4 +1,4 @@
-$(function () {
+$((function () {
   /**
    * Fetches the current admin user for the dashboard.
    *
@@ -211,92 +211,6 @@ $(function () {
   }
 
   /**
-   * Handles the navigation for the admin dashboard by:
-   * - Closing all dropdown menus on mouseleave event for desktop
-   * - Closing all dropdown menus on click event for tablet
-   * - Resetting the toggle buttons on mouseleave event for desktop
-   * - Resetting the toggle buttons on click event for tablet
-   * - Listening for window resize event and re-evaluating the state on change
-   *
-   * @return {void}
-   */
-  function handleNavigation() {
-    const $asideDashboard = $(".aside-dashboard");
-    const $campaignsToggle = $("#campaigns-toggle");
-    const $blogsToggle = $("#blogs-toggle");
-    const $campaignDropdownMenu = $(".campaign-dropdown-menu");
-    const $blogDropdownMenu = $(".blog-dropdown-menu");
-    const $asideToggle = $(".aside-toggle").find(".fa-chevron-left");
-
-    // Desktop media query
-    const desktopMediaQuery = window.matchMedia("(min-width: 992px)");
-
-    // Helper function to close all dropdowns and reset toggles
-    function closeAllDropdowns() {
-      $campaignDropdownMenu.slideUp(200);
-      $blogDropdownMenu.slideUp(200);
-      $campaignsToggle.removeClass("open");
-      $blogsToggle.removeClass("open");
-    }
-
-    // Handle mouseleave event for desktop
-    $asideDashboard.on("mouseleave", function () {
-      if (desktopMediaQuery.matches) {
-        closeAllDropdowns();
-      }
-    });
-
-    // Handle click event for the aside toggle for tablet
-    $asideToggle.on("click", function () {
-      if (!desktopMediaQuery.matches) {
-        closeAllDropdowns();
-      }
-    });
-
-    // Add a listener to re-evaluate on window resize
-    desktopMediaQuery.addEventListener("change", function (event) {
-      if (!event.matches) {
-        closeAllDropdowns();
-      }
-    });
-  }
-
-  /**
-   * Handles the sub navigation of the admin dashboard by:
-   * - Toggling the open class on the campaign and blog toggles
-   * - Sliding up or down the respective campaign and blog dropdown menus
-   *
-   * @return {void}
-   */
-  function handleSubNavigation() {
-    const $campaignToggle = $("#campaigns-toggle");
-    const $blogToggle = $("#blogs-toggle");
-
-    const $campaignDropdown = $campaignToggle.next(".campaign-dropdown-menu");
-    const $blogDropdown = $blogToggle.next(".blog-dropdown-menu");
-
-    if ($campaignToggle.length) {
-      $campaignToggle.on("click", function () {
-        $(this).toggleClass("open");
-
-        if ($campaignDropdown.length) {
-          $campaignDropdown.slideToggle(300);
-        }
-      });
-    }
-
-    if ($blogToggle.length) {
-      $blogToggle.on("click", function () {
-        $(this).toggleClass("open");
-
-        if ($blogDropdown.length) {
-          $blogDropdown.slideToggle(300);
-        }
-      });
-    }
-  }
-
-  /**
    * Formats a number to a human-readable string.
    *
    * Rounds to nearest million, thousand, or leaves as is.
@@ -399,8 +313,6 @@ $(function () {
     const finances = fetchFinances();
     const requests = recentRequests();
     const fundings = pendingFundings();
-    handleSubNavigation(); // Handle sub-navigation toggle
-    handleNavigation(); // Hide sub-navigation
 
     $(".benefactor-name").text(`${admin.firstName}`);
     $("#admin-role").text(
@@ -561,4 +473,4 @@ $(function () {
   }
 
   displayBenefeciaryDashboard();
-});
+}));
